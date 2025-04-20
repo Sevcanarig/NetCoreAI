1d5c98c780d75ac11490003f46264d078c8be9a2
Sevcan Arığ <sevcanarig01@gmail.com>
2025-04-21 00:24:30 +0300

    Initial commit

---
+++ NetCoreAI.Project06_DallEImageGeneration/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string apiKey = "apikey";
+        Console.Write("Çizilmesini istediğiniz içerik (example prompts): ");
+        string prompt;
+        prompt = Console.ReadLine();
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var requestBody = new
+            {
+                prompt = prompt,
+                n = 1,
+                size = "1024x1024"
+            };
+
+            string jsonBody = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/images/generations", content);
+            string responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+        }
+    }
+}
+
+/*
+ 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
+ */
---
+++ NetCoreAI.Project20_RecipeSuggestionWithOpenAI/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project20_RecipeSuggestionWithOpenAI.Models;
+
+namespace NetCoreAI.Project20_RecipeSuggestionWithOpenAI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly OpenAIService _OpenAIService;
+
+        public DefaultController(OpenAIService openAIService)
+        {
+            _OpenAIService = openAIService;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRecipe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipe(string ingredients)
+        {
+            var result = await _OpenAIService.GetRecipeAsync(ingredients);
+            ViewBag.recipe = result;
+            return View();
+        }
+    }
+}
